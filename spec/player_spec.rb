d309e97541fab6c87a81124a4d504e805e0cdb61
App Academy Student <student@appacademy.io>
2014-05-16 18:00:20 -0400

    Began player specs.

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -2,5 +2,29 @@ require 'rspec'
 require 'player'
 
 describe Player do
+  subject(:player) do
+    Player.new('Greg', 1000)
+  end
   
+  describe "#initialize" do
+    it "sets the correct name" do
+      expect(player.name).to eq('Greg')
+    end
+    
+    it "sets the bankroll appropriately" do
+      expect(player.bankroll).to eq(1000)
+    end
+  end
+  
+  describe "#pay_winnings" do
+    
+  end
+  
+  describe "#return_cards" do
+    
+  end
+  
+  describe "#place_bet" do
+    
+  end
 end
